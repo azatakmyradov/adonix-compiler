deed530e8c787432467fd140841f5eb943f024e5
Azat Akmyradov <azat@akmyradov.me>
2023-07-23 06:50:42 -0400

    refactor and add comments

--- main.js
+++ main.js
@@ -1,60 +1,76 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const multer = require('multer');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const multer = require("multer");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const upload = multer({ dest: 'tmp/' });
-const port = process.env.PORT;
+// Default paths
+const upload = multer({ dest: "tmp/" });
 const X3_PATH = process.env.X3_PATH;
-const FOLDERS_PATH = `${X3_PATH}\\folders`;
 const ROOT_PATH = __dirname;
 const X3_URL = process.env.X3_URL;
 
-const Adonix = require('./src/Adonix.js');
-const adx = new Adonix(X3_PATH, FOLDERS_PATH, ROOT_PATH);
+const port = process.env.PORT;
+
+const Adonix = require("./src/Adonix.js");
+const adx = new Adonix(X3_PATH, ROOT_PATH);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+/*
+ * Returns JSON to check if the server is up
+ */
+app.get("/", (req, res) => {
+  res.json({
+    message: "working",
+  });
+});
+
+/*
+ * Returns file names in TRT folder
+ */
+app.get("/:folder/files", (req, res) => {
+  const X3Folder = req.params.folder.toUpperCase();
+
+  adx.files(X3Folder).then((result) => {
     res.json({
-        message: 'working'
+      result,
+      message: "Request was successful",
+      total: result.length,
     });
+  });
 });
 
-app.get('/:folder/files', (req, res) => {
-    adx.files(req.params.folder.toUpperCase()).then(result => {
-        res.json({
-            result,
-            message: 'Request was successful',
-            total: result.length
-        });
-    });
+/*
+ * Returns the file that was requested from the TRT folder
+ */
+app.get("/:folder/download", (req, res) => {
+  const fileName = req.query.fileName.replace(".src", "");
+  const X3Folder = req.params.folder.toUpperCase();
+
+  adx.fetch(X3Folder, fileName).then((data) => {
+    res.sendFile(`${ROOT_PATH}/tmp/${data.fileName}.src`);
+  });
 });
 
-app.get('/:folder/download', (req, res) => {
-    const fileName = req.query.fileName.replace('.src', '');
+/*
+ * Uploads/compiles the file that was received and compiles
+ */
+app.post("/:folder/upload", upload.single("file"), (req, res) => {
+  const fileName = req.file.originalname.replace(".src", "");
+  const X3Folder = req.params.folder.toUpperCase();
 
-    adx.fetch(req.params.folder.toUpperCase(), fileName).then(data => {
-        res.sendFile(`${ROOT_PATH}/tmp/${data.fileName}.src`);
+  adx.sync(X3Folder, req.file.path, fileName).then((result) => {
+    adx.compile(X3Folder, fileName).then((result) => {
+      res.json({
+        message: "uploaded and compiled",
+        result: result,
+        host: X3_URL,
+      });
     });
-});
-
-app.post('/:folder/upload', upload.single('file'), (req, res) => {
-    const fileName = req.file.originalname.replace('.src', '');
-    const folder = req.params.folder.toUpperCase();
-
-    adx.sync(folder, req.file.path, fileName).then(result => {
-        adx.compile(folder, fileName).then(result => {
-            res.json({
-                message: 'uploaded and compiled',
-                result: result,
-                host: X3_URL
-            });
-        });
-    });
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Compiler is listening on port: ${port}`)
+  console.log(`Compiler is listening on port: http://localhost:${port}`);
 });
--- src/Command.js
+++ src/Command.js
@@ -1,17 +1,27 @@
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
 
 class Command {
-    static execute(command, callback) {
-        return new Promise((resolve, reject) => {
-            exec(command,  { maxBuffer: 1024 * 5000 }, function (error, stdout, stderr) {
-                const result = callback(stdout);
-    
-                console.log(stderr);
-    
-                resolve(result);
-            });
-        });
-    }
+  /*
+   * Run system commands
+   *
+   * @param command Command to be executed
+   * @param callback Callback to handle the result
+   */
+  static execute(command, callback) {
+    return new Promise((resolve, reject) => {
+      exec(
+        command,
+        { maxBuffer: 1024 * 5000 },
+        function (error, stdout, stderr) {
+          const result = callback(stdout);
+
+          console.log(stderr);
+
+          resolve(result);
+        }
+      );
+    });
+  }
 }
 
 module.exports = Command;
